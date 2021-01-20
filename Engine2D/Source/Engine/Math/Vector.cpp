d97622b1583bb143f7bcb096a06c447cfc1c84c5
Victor Val <victor.val@avenuecode.com>
2021-01-20 14:17:11 -0300

    Improving the vect class with math operations

--- Engine2D/Source/Engine/Math/Vector.cpp
+++ Engine2D/Source/Engine/Math/Vector.cpp
@@ -1,5 +1,7 @@
 #include "Engine\Math\Vector.h"
 
+#include <cmath>
+
 Vector::Vector() {
 	x = 0.0f;
 	y = 0.0f;
@@ -23,4 +25,59 @@ Vector & Vector::operator=(const Vector & other){
 
 Vector::~Vector(){
 
+}
+
+
+Vector Vector::operator+(const Vector& a) {
+	return Vector(x + a.x, y + a.y);
+}
+Vector Vector::operator-(const Vector& a) {
+	return Vector(x - a.x, y - a.y);
+}
+Vector Vector::operator*(const float& a) {
+	return Vector(x*a, y*a);
+}
+Vector Vector::operator/(const float& a) {
+	return Vector(x / a, y / a);
+}
+
+
+bool Vector::operator==(const Vector& a) {
+	return x == a.x && y == a.y;
+}
+
+
+// Assignment operators
+void Vector::operator+=(const Vector& a) {
+	x += a.x; y += a.y;
+}
+void Vector::operator-=(const Vector& a) {
+	x -= a.x; y -= a.y;
+}
+void Vector::operator*=(const float& a) {
+	x *= a; y *= a;
+}
+void Vector::operator/=(const float& a) {
+	x /= a; y /= a;
+}
+
+
+float Vector::Length() {
+	return sqrt(x*x + y * y);
+}
+
+void Vector::Normalize() {
+	float l = Length();
+	if (l > 0.0f) {
+		x /= l;
+		y /= l;
+	}
+}
+
+Vector Vector::Normalized() {
+	float l = Length();
+	if (l > 0.0f) {
+		return Vector(x / l, y / l);
+	}
+	return Vector(0.0f, 0.0f);
 }
--- Engine2D/Source/Engine/Math/Vector.h
+++ Engine2D/Source/Engine/Math/Vector.h
@@ -8,6 +8,26 @@ public:
 	Vector(const Vector& other);
 	Vector& operator=(const Vector& other);
 	virtual ~Vector();
+	
+	Vector operator+(const Vector& a);
+	Vector operator-(const Vector& a);
+	Vector operator*(const float& a);
+	Vector operator/(const float& a);
+
+	bool operator==(const Vector& a);
+
+	// Assignment operators
+	void operator+=(const Vector& a);
+	void operator-=(const Vector& a);
+	void operator*=(const float& a);
+	void operator/=(const float& a);
+
+	// Retorna o comprimento do vetor (sqrt(x*x+y*y)) 
+	float Length();
+	// Normaliza o Vetor (divide x e y pelo comprimento)
+	void Normalize();
+	// Retorna uma cópia normalizada do Vetor
+	Vector Normalized();
 
 	union {
 		float x, u, s;
