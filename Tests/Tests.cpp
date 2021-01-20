a5c52c85d6e9b3959eb843c65007564878615c06
bernardo-senna <bermullet@hotmail.com>
2021-01-20 10:19:58 -0300

    Adição do projeto de testes vazio

---
+++ Tests/Tests.cpp
@@ -0,0 +1,24 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Engine2D/Source/Engine/App.h"
+#include "../Engine2D/Source/Engine/Component.h"
+#include "../Engine2D/Source/Engine/Engine.h"
+#include "../Engine2D/Source/Engine/Entity.h"
+#include "../Engine2D/Source/Engine/Components/Image2D.h"
+#include "../Engine2D/Source/Engine/Components/Transform2D.h"
+#include "../Engine2D/Source/Engine/Math/Vector.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Tests
+{
+	TEST_CLASS(Tests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+
+		}
+	};
+}
