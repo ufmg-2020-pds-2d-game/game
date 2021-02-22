1aaadba80796daf7d8bd1376b548a0c51b745a8e
Uniday Studio <guilhermegt13@gmail.com>
2021-02-22 11:36:20 -0300

    Preparando o Mock do Component para os testes

--- Engine2DTest/TestMocks.h
+++ Engine2DTest/TestMocks.h
@@ -7,22 +7,38 @@
 
 class TestComponentVirtualMethods : public Component {
 public:
-    TestComponentVirtualMethods() {
+	TestComponentVirtualMethods() {
+		calledStart = nullptr;
+		calledUpdate = nullptr;
+		calledEnd = nullptr;
+	}
+	virtual ~TestComponentVirtualMethods() {
 
-    }
-    virtual ~TestComponentVirtualMethods() {
+	}
 
-    }
+	// Ponteiros para três booleanas. Eles são setados
+	// como true (caso não nulos) assim que o App chama
+	// cada um dessas funções.
+	bool* calledStart;
+	bool* calledUpdate;
+	bool* calledEnd;
 
-    virtual void Start() override {
-
-    }
-    virtual void Update() override {
-
-    }
-    virtual void End() override {
-
-    }
+	// Aqui as três funções sobrescritas para fazer isso:
+	virtual void Start() override {
+		if (calledStart) {
+			*calledStart = true;
+		}
+	}
+	virtual void Update() override {
+		if (calledUpdate) {
+			*calledUpdate = true;
+		}
+	}
+	virtual void End() override {
+		if (calledEnd) {
+			*calledEnd = true;
+		}
+	}
 };
 
 #endif // !TEST_MOCKS_H
